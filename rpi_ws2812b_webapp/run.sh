cff5e8afd7ad5e07bb8dc00835a6a634e598b28d
Théo <theo.cheynel@gmail.com>
2022-06-12 20:20:28 +0200

    Exporting variables in run.sh

--- rpi_ws2812b_webapp/run.sh
+++ rpi_ws2812b_webapp/run.sh
@@ -1,2 +1,5 @@
+export SPOTIPY_CLIENT_ID=2a785c1268df422e8a79da540e4ae6d7
+export SPOTIPY_CLIENT_SECRET=876acd18b7d14023be497db8421b3cf6
+export SPOTIPY_REDIRECT_URI=http://example.com
 export FLASK_APP=main.py
 python3 -m flask run --host 0.0.0.0
